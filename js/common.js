dccd4f042a252f5e6740ca4063ccf13fd89b1775
Gurwinder Singh <gurwinder@unl.global>
2023-03-30 11:14:45 +0530

    Updated docs

---
+++ js/common.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+    // $("#api-ref").click(function(event) {
+    //     const element = document.getElementById('menu-drop-list');
+    //     if(element.style.display.toString() === 'block') {
+    //         document.getElementById('menu-drop-list').style.display = 'none';
+    //     }
+    //     else {
+    //         document.getElementById('menu-drop-list').style.display = 'block';
+
+    //     }
+    // });
+
+});
+  
+// function onScroll(event){
+//     var scrollPos = $(document).scrollTop();
+//     $('.scrollitem a').each(function () {
+//         var currLink = $(this);
+//         // console.log(currLink);
+//         var refElement = $(currLink.attr("href"));
+//         // console.log('refElement', refElement)
+//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+//             console.log('>>>>yes', currLink)
+//             $('.scrollitem li a').removeClass("link--blue");
+//             currLink.addClass("link--blue");
+//         }
+//         else{
+//             console.log('>>>>>no')
+//             currLink.removeClass("link--blue");
+//             currLink.addClass("link--gray");
+//         }
+//     });
+// }
